8b9b7c9baae15ffc5816e2c58225c9605c535692
unknown <vst87@outlook.com>
2021-08-23 23:50:36 +0530

    react-complete

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -9,7 +9,7 @@ function Expenses() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.59, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
